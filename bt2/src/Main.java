def3e6cb37429d006d02ad74d3a7a625fc39aadf
unknown <trinhnam180697@gmail.com>
2023-01-10 05:20:21 +0700

    mini test w1 md2

--- bt2/src/Main.java
+++ bt2/src/Main.java
@@ -1,18 +1,33 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int[] arr = {1, 5, 3, 2, 6, 4, 9, 8, 7, 10, 12, 20, 45, 32, 0, 11};
-        System.out.print("arr : ");
+        System.out.println("arr: " + Arrays.toString(arr));
+
+        int sum = sumArr(arr);
+        System.out.println("sum arr = " + sum);
+
+        int max = maxArr(arr);
+        System.out.println("max arr = " + max);
+
+    }
+
+    public static int sumArr(int[] arr) {
         int sum = 0;
-        int max = arr[0];
         for (int i : arr) {
             sum += i;
-            System.out.print(i + " ");
+        }
+        return sum;
+    }
+
+    public static int maxArr(int[] arr) {
+        int max = arr[0];
+        for (int i : arr) {
             if (max < i) {
                 max = i;
             }
         }
-        System.out.println(" ");
-        System.out.println("sum arr = " + sum);
-        System.out.println("max arr = " + max);
+        return max;
     }
 }
